72eb3a4853ce210198d02c62c2db47c128b01f21
avi-spc <avdhesh7394vn@gmail.com>
2022-07-29 10:27:27 +0530

    created filtered brands object from searched objects, stored values for filter checkbox if checked

--- src/components/products_search/ProductFilter.js
+++ src/components/products_search/ProductFilter.js
@@ -1,17 +1,62 @@
+import { useContext, useEffect, useState } from "react";
+
+import { ProductsContext } from "../../contexts/productsContext";
+
 const ProductFilter = () => {
+    const [brands, setBrands] = useState(null);
+    const [selectedBrands, setSelectedBrands] = useState([]);
+
+    const { products } = useContext(ProductsContext);
+
+    useEffect(() => {
+        if (products.length) {
+            const brandSet = new Set(
+                products.map((product) => {
+                    return product.brand;
+                })
+            );
+
+            setBrands(Array.from(brandSet));
+        }
+    }, [products]);
+
+    useEffect(() => {
+        console.log(selectedBrands);
+    }, [selectedBrands]);
+
+    const handleBrands = (e) => {
+        if (e.target.checked) {
+            setSelectedBrands([...selectedBrands, e.target.value]);
+        } else {
+            setSelectedBrands(
+                selectedBrands.filter((uncheckedBrand) => {
+                    return uncheckedBrand !== e.target.value;
+                })
+            );
+        }
+    };
+
     return (
         <section className="product-filter">
             <div className="filter-heading tertiary-heading">Filter</div>
             <div className="product-filter__category">
                 <div className="product-filter__category--name">Brand</div>
-                <div className="filter-option">
-                    <input type="checkbox" name="Countrybean" />
-                    <label htmlFor="Countrybean">Countrybean</label>
-                </div>
-                <div className="filter-option">
-                    <input type="checkbox" name="Nestle" />
-                    <label htmlFor="Nestle">Nestle</label>
-                </div>
+                {brands &&
+                    brands.map((brand, index) => {
+                        return (
+                            <div className="filter-option" key={index}>
+                                <input
+                                    type="checkbox"
+                                    id={brand}
+                                    name={brand}
+                                    value={brand}
+                                    onChange={(e) => handleBrands(e)}
+                                />
+                                <label htmlFor={brand}>{brand}</label>
+                            </div>
+                        );
+                    })}
+
                 <div className="product-filter__category--name">Price</div>
                 <div className="filter-option">
                     <input type="checkbox" name="Countrybean" />
